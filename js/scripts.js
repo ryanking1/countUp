1b0b0dc3a40dfaabafbb505b1df2f02a20d4eee1
Chris Kuiper and Ryan King <ryanking157@hotmail.com>
2016-01-21 14:43:18 -0800

    add functionality to form

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,29 @@
-
-var countUpBy = function(endCount, incrementor) {
-  var outputArray = [];
-
-  if(isNaN(endCount) || isNaN(incrementor)) {
-    outputArray = false;
-  }else if ((endCount || incrementor) === 0) {
-    outputArray = false;
-  }else {
-    for(var i = incrementor; i <= endCount; i += incrementor) {
-      outputArray.push(i);
-    }
-  }
-  return outputArray;
-}
-
-
-
-
-/*$(function() {
+$(function() {
   $("#form").submit(function(event) {
-  var endCount = parseInt($("#endCount").val());
-  var incrementor = parseInt($("#incrementor").val());
-
-  event.preventDefault();
- });
-});*/
+    var endCount = parseInt($("#endCount").val());
+    var incrementor = parseInt($("#incrementor").val());
+    var countUpBy = function(endCount, incrementor) {
+      var outputArray = [];
+      if(endCount < incrementor) {
+        outputArray = false;
+      } else if(isNaN(endCount) || isNaN(incrementor)) {
+        outputArray = false;
+      } else if ((endCount || incrementor) === 0) {
+        outputArray = false;
+      } else {
+        for(var i = incrementor; i <= endCount; i += incrementor) {
+          outputArray.push(i);
+        }
+      }
+      return outputArray;
+    }
+    var outputArray = countUpBy(endCount, incrementor);
+    if (outputArray === false) {
+      alert("Please enter a different number");
+    }
+    else {
+      $("#appendList").text(outputArray.toString());
+    }
+    event.preventDefault();
+  });
+});
