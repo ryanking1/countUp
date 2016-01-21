3770f8890ed981d83d7540cefe3fe4ef1779735c
Chris Kuiper and Ryan King <chirs.kuiper@gmail.com>
2016-01-21 13:23:20 -0800

    create specs for false satements

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
-$(function() {
+
+var countUpBy = function(endCount, incrementor) {
+  var outputArray = [];
+
+  if(isNaN(endCount) || isNaN(incrementor)) {
+    outputArray = false;
+  }else if ((endCount || incrementor) === 0) {
+    outputArray = false;
+  }else {
+    for(var i = incrementor; i <= endCount; i += incrementor) {
+      outputArray.push(i);
+    }
+  }
+  return outputArray;
+}
+
+
+
+
+/*$(function() {
   $("#form").submit(function(event) {
   var endCount = parseInt($("#endCount").val());
   var incrementor = parseInt($("#incrementor").val());
 
-
-  for(i = 0; i <= endCount; i += incrementor) {
-    if (endCount > 0) {
-      $("#appendList").append(i + ", ");
-    }
-    else {
-      alert("Please choose a number greater than 0");
-    }
-  }
-
   event.preventDefault();
-
  });
-});
+});*/
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,17 @@
-describe("countUp", function() {
-  it("sets input value to count to", function() {
-    expect(endCount).to.equal(50);
+describe("countUpBy", function() {
+  it("set input value to count to", function() {
+    expect(countUpBy(5,1)).to.eql([1,2,3,4,5]);
   });
-  it("sets incrementor value to count by", function(){
-    expect(incrementor).to.equal(5);
+  it("count up by a number other than 1", function() {
+    expect(countUpBy(10,5)).to.eql([5,10]);
+  });
+  it("stops counting at the correct number", function() {
+    expect(countUpBy(14,5)).to.eql([5,10]);
+  });
+  it("alerts user if input is not a number", function() {
+    expect(countUpBy("rt","rt")).to.equal(false);
+  });
+  it("alerts user if zero is entered", function() {
+    expect(countUpBy(0,0)).to.equal(false);
   });
 });
